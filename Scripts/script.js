127748b54dd6c36c67011cf3a24f3ef3b0957a3a
tahataha005 <taha.f.taha0@gmail.com>
2024-06-27 17:53:33 +0300

    creating form submission

---
+++ Scripts/script.js
@@ -0,0 +1,27 @@
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const textInput = document.getElementById("message");
+
+const submitButton = document.getElementById("submit");
+const response = document.getElementById("formResponse");
+
+submitButton.addEventListener("click", function (e) {
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const text = textInput.value;
+
+  if (name !== "" && email !== "") {
+    const reponseObj = {
+      name: name,
+      email: email,
+      text: text,
+    };
+
+    console.log(reponseObj);
+
+    response.innerText = "Form Submitted!";
+    e.preventDefault();
+  } else {
+    response.innerText = "Fill all required fields";
+  }
+});
